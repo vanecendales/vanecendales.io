06e37e82214ee890fd4b0dddd25bff40812ee425
Vanessa Cendales <vanecendales@gmail.com>
2020-06-25 10:28:50 -0500

    missing js and anteroir folder

---
+++ sandra&oscar/mailer.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['submit'])) {
+
+	$to = "sandritagalan@hotmail.com,vanecendales@gmail.com,oscar_eagle47@hotmail.com"; 
+	$subject = "Confirmación a la boda";
+	$name_field = $_POST['name'];
+	$message = $_POST['message'];
+	$option = $_POST['radio'];
+
+	$body = "Invitado: $name_field\n  $check_msg Option: $option\n Comentarios:\n $message\n";
+
+	echo "<div style='background: #0d0d2c; width: 100%;height: 100%;color: white;padding-top: 50%;
+    text-align: center; font-size: 3rem; font-family: helvetica; font-weight: lighter;'>Gracias por tu confirmaci&oacute;n! </div>";
+	mail($to, $subject, $body);
+	
+} else {
+	echo "blarg!";
+}
+?>
+
+
+
+
---
+++ var.psc
@@ -0,0 +1,13 @@
+Algoritmo holamundo
+	Definir edad como entero;
+	definir nota Como Real;
+	definir nombrecompelto como cadena;
+	definir encontrado Como Logico;
+	
+	nota <- 4.5;
+	edad <- 34;
+	nombrecompleto <- "vnessa cendales"
+	encontrado<- Verdadero
+	
+	leer nota;
+FinAlgoritmo
